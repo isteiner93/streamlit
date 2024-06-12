c0b0586bb3ed560e2e88170b80c94f478b7d9899
Ivan Steiner <59017725+isteiner93@users.noreply.github.com>
2024-06-12 17:23:36 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+    st.title("Julka, prosim zavezies nas na kviz?")
+
+    # Displaying the image of the cat
+    cat_image = "https://i.imgflip.com/5wdad.jpg?a477120"
+
+if __name__ == "__main__":
+    main()
